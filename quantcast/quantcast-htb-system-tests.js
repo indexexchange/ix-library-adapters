d8609ea8e9f9741fa4ac1c35df931faf93865e64
Amir Sabbaghi <amir.sabbaghi@quantcast.com>
2019-03-19 10:26:00 +0000

    initial quantcast wrapper

---
+++ quantcast/quantcast-htb-system-tests.js
@@ -0,0 +1,117 @@
+'use strict';
+
+function getPartnerId() {
+    return 'QuantcastHtb';
+}
+
+function getStatsId() {
+    return 'QUC';
+}
+
+function getBidRequestRegex() {
+    return {
+        method: 'POST',
+        urlRegex: /rtbtest\.rtb\.quantserve\.net:8...\/qchb/
+    };
+}
+
+function getCallbackType() {
+    return 'NONE';
+}
+
+function getArchitecture() {
+    return 'MRA';
+}
+
+function getConfig() {
+    return {
+        publisherId: "",
+        xSlots: {
+            1: {
+                placementCode: "123",
+                sizes: [{
+                    width: 300,
+                    height: 250
+                }]
+            }
+        }
+    };
+}
+
+function validateBidRequest(request) {
+    var r = JSON.parse(request.body);
+
+    expect(r.publisherId).toBeDefined();
+    expect(r.requestId).toBeDefined();
+
+    expect(r.imp.length).toBe(1);
+
+    expect(r.imp[0]).toEqual({
+        banner: {
+            battr: [3],
+            sizes: [{
+                height: 250,
+                width: 300
+            }],
+            pos: 1
+        },
+        placementCode: "123",
+        bidFloor: "2"
+    });
+}
+
+function getValidResponse(request, creative) {
+    var r = JSON.parse(request.body);
+    var adm = creative || "<h1>Buy now!</h2>";
+    var response = {
+        "bidderCode": "quantcast",
+        "bids":[{
+            "ad": adm,
+            "cpm": 1,
+            "creativeId": 0,
+            "currency": "USD",
+            "placementCode": r.imp[0].placementCode,
+            "statusCode": 1,
+            "width": r.imp[0].banner.sizes[0].width,
+            "height": r.imp[0].banner.sizes[0].height
+        }]
+    };
+    return JSON.stringify(response);
+}
+
+function getPassResponse(request) {
+    var r = JSON.parse(request.body);
+    var response = {
+        "bidderCode": "quantcast",
+        "bids":[{
+            "placementCode": r.imp[0].placementCode,
+            "statusCode": 0,
+            "width": r.imp[0].banner.sizes[0].width,
+            "height": r.imp[0].banner.sizes[0].height
+        }]
+    };
+    return JSON.stringify(response);
+}
+
+function validateTargeting(targetingMap) {
+    expect(targetingMap).toEqual(jasmine.objectContaining({
+        ix_qua_id: jasmine.arrayContaining([jasmine.any(String)])
+    }));
+}
+
+function validatePixelRequests(pixelRequests) {
+}
+
+module.exports = {
+    getPartnerId: getPartnerId,
+    getStatsId: getStatsId,
+    getBidRequestRegex: getBidRequestRegex,
+    getCallbackType: getCallbackType,
+    getArchitecture: getArchitecture,
+    getConfig: getConfig,
+    getPassResponse: getPassResponse,
+    validateBidRequest: validateBidRequest,
+    getValidResponse: getValidResponse,
+    validateTargeting: validateTargeting,
+    validatePixelRequests: validatePixelRequests
+};
