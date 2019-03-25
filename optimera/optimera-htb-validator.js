221b980addc668deceb348d73ede0ae85a3642e4
mcallari <mike.callari@gmail.com>
2019-03-25 10:46:46 -0400

    Moving Optimera to new repo.

---
+++ optimera/optimera-htb-validator.js
@@ -0,0 +1,62 @@
+/**
+ * @author:    Partner
+ * @license:   UNLICENSED
+ *
+ * @copyright: Copyright (c) 2017 by Index Exchange. All rights reserved.
+ *
+ * The information contained within this document is confidential, copyrighted
+ * and or a trade secret. No part of this document may be reproduced or
+ * distributed in any form or by any means, in whole or in part, without the
+ * prior written permission of Index Exchange.
+ */
+
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// Dependencies ////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+var Inspector = require('../../../libs/external/schema-inspector.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// Main ////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+/* =============================================================================
+ * STEP 0 | Config Validation
+ * -----------------------------------------------------------------------------
+ * This file contains the necessary validation for the partner configuration.
+ * This validation will be performed on the partner specific configuration object
+ * that is passed into the wrapper. The wrapper uses an outside library called
+ * schema-insepctor to perform the validation. Information about it can be found here:
+ * https://atinux.fr/schema-inspector/.
+ */
+var partnerValidator = function (configs) {
+    var result = Inspector.validate({
+        type: 'object',
+        properties: {
+            clientID: {
+                type: "integer",
+                gte: 0
+            },
+            xSlots: {
+                type: 'object',
+                properties: {
+                    '*': {
+                        type: 'object'
+                    }
+                }
+            }
+        }
+    }, configs);
+
+    if (!result.valid) {
+        return result.format();
+    }
+
+    return null;
+};
+
+module.exports = partnerValidator;
