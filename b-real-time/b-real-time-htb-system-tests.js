c522019d0ec33325800f8929904f0ef16f9963c1
Nick Colletti <nick.colletti@emxdigital.com>
2019-07-09 11:49:12 -0400

    updates to pass tests. added missing tests. reformatting of parseResponse. added pixel url

--- b-real-time/b-real-time-htb-system-tests.js
+++ b-real-time/b-real-time-htb-system-tests.js
@@ -8,13 +8,6 @@ function getStatsId() {
     return 'BRT';
 }
 
-function getBidRequestRegex() {
-    return {
-        method: 'POST',
-        urlRegex: /.*hb\.emxdgt\.com\/.?t=\d*&ts=\d*/
-    };
-}
-
 function getCallbackType() {
     return 'NONE';
 }
@@ -27,23 +20,29 @@ function getConfig() {
     return {
         xSlots: {
             1: {
-                tagid: 25251,
+                tagid: 74072,
                 bidfloor: 0.01,
                 sizes: [[300, 250]]
             },
             2: {
-                tagid: 25251,
+                tagid: 74072,
                 bidfloor: 0.01,
-                sizes: [[320, 50]]
+                sizes: [[728, 90]]
             }
         }
     };
 }
 
+function getBidRequestRegex() {
+    return {
+        method: 'POST',
+        urlRegex: /.*hb\.emxdgt\.com\/.?t=\d*&ts=\d*/
+    };
+}
+
 function validateBidRequest(request) {
     var r = JSON.parse(request.body);
-
-    expect(r.id).toBeDefined();
+    expect(request.body).toBeDefined();
 
     expect(r.site.page).toBeDefined();
     expect(r.site.domain).toBeDefined();
@@ -51,70 +50,126 @@ function validateBidRequest(request) {
     expect(r.imp.length).toBe(2);
 
     expect(r.imp[0]).toEqual({
-        id: r.requestId,
+        id: r.imp[0].id,
         banner: {
+            format: [{
+                w: 300,
+                h: 250
+            }],
             w: 300,
-            h: 250,
-            format: [
-                {
-                    w: 300,
-                    h: 250
-                }
-            ]
+            h: 250
         },
-        secure: 0,
-        tagid: 123
+        tagid: 74072,
+        secure: r.imp[0].secure,
+        bidfloor: 0.01
     });
 
     expect(r.imp[1]).toEqual({
-        id: r.requestId,
+        id: r.imp[1].id,
         banner: {
-            w: 320,
-            h: 50,
-            format: [
-                {
-                    w: 320,
-                    h: 50
-                }
-            ]
+            format: [{
+                w: 728,
+                h: 90
+            }],
+            w: 728,
+            h: 90
         },
-        secure: 0,
-        tagid: 123
+        tagid: 74072,
+        secure: r.imp[1].secure,
+        bidfloor: 0.01
     });
 }
 
-function getValidResponse(request) {
+function getValidResponse(request, creative) {
     var r = JSON.parse(request.body);
+
     var response = {
         id: r.id,
-        seatbid: [
-            {
-                bid: [
-                    {
-                        adm: 'this is a bid from emx!',
-                        id: '104e73d56709256',
-                        ttl: 300,
-                        crid: '41975016',
-                        w: 300,
-                        price: 11.3645,
-                        adid: '41975016',
-                        h: 250
-                    },
-                    {
-                        adm: 'this is a bid from emx!',
-                        id: '11ee309e23efab5',
-                        ttl: 300,
-                        crid: '41975016',
-                        w: 320,
-                        price: 11.3645,
-                        adid: '41975016',
-                        h: 50
-                    }
-                ],
-                seat: '2439'
-            }
-        ]
+        seatbid: [{
+            bid: [{
+                adm: creative || '<div id="1"></div>',
+                id: r.imp[0].id,
+                ttl: 300,
+                crid: "94395500",
+                w: 300,
+                price: 11.3645,
+                adid: "94395500",
+                h: 250
+            }],
+            seat: "1356"
+        }, {
+            bid: [{
+                adm: creative || '<div id="2"></div>',
+                id: r.imp[1].id,
+                ttl: 300,
+                crid: "41975016",
+                w: 728,
+                price: 11.3645,
+                adid: "41975016",
+                h: 90
+            }],
+            seat: "1356"
+        }]
     };
+
+    return JSON.stringify(response);
+}
+
+function getValidResponseWithDeal(request, creative) {
+    var r = JSON.parse(request.body);
+
+    var response = {
+        id: r.id,
+        seatbid: [{
+            bid: [{
+                adm: creative || '<div id="1"></div>',
+                id: r.imp[0].id,
+                ttl: 300,
+                crid: "94395500",
+                w: 300,
+                price: 11.3645,
+                adid: "94395500",
+                h: 250,
+                dealid: 11
+            }],
+            seat: "1356"
+        }, {
+            bid: [{
+                adm: creative || '<div id="2"></div>',
+                id: r.imp[1].id,
+                ttl: 300,
+                crid: "41975016",
+                w: 728,
+                price: 11.3645,
+                adid: "41975016",
+                h: 90,
+                dealid: 12
+            }],
+            seat: "1356"
+        }]
+    };
+
+    return JSON.stringify(response);
+}
+
+
+function validateTargeting(targetingMap) {
+    expect(targetingMap).toEqual(jasmine.objectContaining({
+        ix_brt_cpm: jasmine.arrayContaining(['300x250_1135', '728x90_1135']),
+        ix_brt_id: jasmine.arrayWithExactContents([jasmine.any(String), jasmine.any(String)])
+    }));
+}
+
+function validateTargetingWithDeal(targetingMap) {
+    expect(targetingMap).toEqual(jasmine.objectContaining({
+        ix_brt_cpm: jasmine.arrayWithExactContents(['300x250_1135', '728x90_1135']),
+        ix_brt_dealid: jasmine.arrayContaining(['300x250_11', '728x90_12']),
+        ix_brt_id: jasmine.arrayWithExactContents([jasmine.any(String), jasmine.any(String)])
+    }));
+}
+
+function getPassResponse(request) {
+    var response = { request: request };
 
     return JSON.stringify(response);
 }
@@ -126,6 +181,10 @@ module.exports = {
     getCallbackType: getCallbackType,
     getArchitecture: getArchitecture,
     getConfig: getConfig,
+    validateTargeting: validateTargeting,
     validateBidRequest: validateBidRequest,
-    getValidResponse: getValidResponse
+    getValidResponse: getValidResponse,
+    getPassResponse: getPassResponse,
+    getValidResponseWithDeal: getValidResponseWithDeal,
+    validateTargetingWithDeal: validateTargetingWithDeal
 };
--- b-real-time/b-real-time-htb.js
+++ b-real-time/b-real-time-htb.js
@@ -1,15 +1,3 @@
-/**
- * @author:    Partner
- * @license:   UNLICENSED
- *
- * @copyright: Copyright (c) 2017 by Index Exchange. All rights reserved.
- *
- * The information contained within this document is confidential, copyrighted
- * and or a trade secret. No part of this document may be reproduced or
- * distributed in any form or by any means, in whole or in part, without the
- * prior written permission of Index Exchange.
- */
-
 'use strict';
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -23,17 +11,16 @@ var Partner = require('partner.js');
 var Size = require('size.js');
 var SpaceCamp = require('space-camp.js');
 var System = require('system.js');
-var Network = require('network.js');
-var Utilities = require('utilities.js');
-var Whoopsie = require('whoopsie.js');
-var EventsService;
-var RenderService;
+
 var ComplianceService;
+var EventsService; // eslint-disable-line no-unused-vars
+var RenderService;
 
 //? if (DEBUG) {
 var ConfigValidators = require('config-validators.js');
 var PartnerSpecificValidator = require('b-real-time-htb-validator.js');
 var Scribe = require('scribe.js');
+var Whoopsie = require('whoopsie.js');
 //? }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -77,6 +64,20 @@ function BRealTimeHtb(configs) {
     /* =====================================
      * Functions
      * ---------------------------------- */
+    function __getMatchingBid(parcel, bidResponse) {
+        if (!bidResponse.hasOwnProperty('seatbid')) {
+            return false;
+        }
+        for (var emxBid of bidResponse.seatbid) {
+            var bid = emxBid.bid[0];
+
+            if (parcel.xSlotName == bid.id) {
+                return bid;
+
+            }
+        }
+        return false;
+    }
 
     /* Utilities
      * ---------------------------------- */
@@ -90,7 +91,7 @@ function BRealTimeHtb(configs) {
      * @return {object}
      */
     function __generateRequestObj(returnParcels) {
-        var timeout = 2000;
+        var timeout = SpaceCamp.globalTimeout || 1500;
         var version = '2.0.0';
 
         var timestamp = System.now();
@@ -107,58 +108,6 @@ function BRealTimeHtb(configs) {
          * STEP 2  | Generate Request URL
          * -----------------------------------------------------------------------------
          *
-         * Generate the URL to request demand from the partner endpoint using the provided
-         * returnParcels. The returnParcels is an array of objects each object containing
-         * an .xSlotRef which is a reference to the xSlot object from the partner configuration.
-         * Use this to retrieve the placements/xSlots you need to request for.
-         *
-         * If your partner is MRA, returnParcels will be an array of length one. If your
-         * partner is SRA, it will contain any number of entities. In any event, the full
-         * contents of the array should be able to fit into a single request and the
-         * return value of this function should similarly represent a single request to the
-         * endpoint.
-         *
-         * Return an object containing:
-         * queryUrl: the url for the request
-         * data: the query object containing a map of the query string paramaters
-         *
-         * callbackId:
-         *
-         * arbitrary id to match the request with the response in the callback function. If
-         * your endpoint supports passing in an arbitrary ID and returning it as part of the response
-         * please use the callbackType: Partner.CallbackTypes.ID and fill out the adResponseCallback.
-         * Also please provide this adResponseCallback to your bid request here so that the JSONP
-         * response calls it once it has completed.
-         *
-         * If your endpoint does not support passing in an ID, simply use
-         * Partner.CallbackTypes.CALLBACK_NAME and the wrapper will take care of handling request
-         * matching by generating unique callbacks for each request using the callbackId.
-         *
-         * If your endpoint is ajax only, please set the appropriate values in your profile for this,
-         * i.e. Partner.CallbackTypes.NONE and Partner.Requesttypes.AJAX. You also do not need to provide
-         * a callbackId in this case because there is no callback.
-         *
-         * The return object should look something like this:
-         * {
-         *     url: 'http://bidserver.com/api/bids' // base request url for a GET/POST request
-         *     data: { // query string object that will be attached to the base url
-         *        slots: [
-         *             {
-         *                 tagid: '54321',
-         *                 sizes: [[300, 250]]
-         *             },{
-         *                 tagid: '12345',
-         *                 sizes: [[300, 600]]
-         *             },{
-         *                 tagid: '65432',
-         *                 sizes: [[728, 90]]
-         *             }
-         *         ],
-         *         site: 'http://google.com'
-         *     }
-         * }
-         */
-
         /* PUT CODE HERE */
 
         var __emxBids = [];
@@ -166,26 +115,24 @@ function BRealTimeHtb(configs) {
         for (var i = 0; i < returnParcels.length; i++) {
             var returnParcel = returnParcels[i];
             var emxBid = {};
-            var uuid = System.generateUniqueId();
             var bidFloor = returnParcel.xSlotRef.bidfloor;
 
-            emxBid.id = returnParcel.requestId;
+            emxBid.id = returnParcels[i].xSlotName;
+            emxBid.tagid = returnParcel.xSlotRef.tagid;
+            emxBid.secure = networkProtocol;
+            if (bidFloor > 0) {
+                emxBid.bidfloor = bidFloor;
+            }
             emxBid.banner = {
-                format: returnParcel.xSlotRef.sizes.map(function (obj) {
+                format: returnParcel.xSlotRef.sizes.map(function (size) {
                     return {
-                        w: obj[0],
-                        h: obj[1]
+                        w: size[0],
+                        h: size[1]
                     };
                 }),
                 w: returnParcel.xSlotRef.sizes[0][0],
                 h: returnParcel.xSlotRef.sizes[0][1]
             };
-            emxBid.tagid = returnParcel.xSlotRef.tagid;
-            emxBid.secure = networkProtocol;
-            emxBid.tid = uuid;
-            if (bidFloor > 0) {
-                emxBid.bidfloor = bidFloor;
-            }
 
             __emxBids.push(emxBid);
         }
@@ -195,7 +142,8 @@ function BRealTimeHtb(configs) {
             imp: __emxBids,
             site: {
                 domain: pageHost,
-                page: pageUrl
+                page: pageUrl,
+                ref: Browser.getReferrer()
             },
             ext: {
                 ver: version
@@ -234,6 +182,23 @@ function BRealTimeHtb(configs) {
         };
     }
 
+    /* =============================================================================
+     * STEP 5  | Rendering Pixel
+     * -----------------------------------------------------------------------------
+     *
+    */
+
+    /**
+     * This function will render the pixel given.
+     * @param  {string} pixelUrl Tracking pixel img url.
+     */
+    function __renderPixel(pixelUrl) {
+        if (pixelUrl) {
+            Browser.createHiddenIFrame(pixelUrl);
+        }
+    }
+
+
     /**
      * Parses and extracts demand from adResponse according to the adapter and then attaches it
      * to the corresponding bid's returnParcel in the correct format using targeting keys.
@@ -247,71 +212,42 @@ function BRealTimeHtb(configs) {
      * generateRequestObj to signal which slots need demand. In this funciton, the demand needs to be
      * attached to each one of the objects for which the demand was originally requested for.
      */
+
     function __parseResponse(sessionId, adResponse, returnParcels) {
-        /* =============================================================================
-         * STEP 4  | Parse & store demand response
-         * -----------------------------------------------------------------------------
-         *
-         * Fill the below variables with information about the bid from the partner, using
-         * the adResponse variable that contains your module adResponse.
-         */
-
-        /* This an array of all the bids in your response that will be iterated over below. Each of
-         * these will be mapped back to a returnParcel object using some criteria explained below.
-         * The following variables will also be parsed and attached to that returnParcel object as
-         * returned demand.
-         *
-         * Use the adResponse variable to extract your bid information and insert it into the
-         * bids array. Each element in the bids array should represent a single bid and should
-         * match up to a single element from the returnParcel array.
-         *
-         */
-
-        /* ---------- Process adResponse and extract the bids into the bids array ------------ */
-
-        var bids = adResponse.seatbid;
-
-        /* --------------------------------------------------------------------------------- */
 
         for (var j = 0; j < returnParcels.length; j++) {
             var curReturnParcel = returnParcels[j];
+            var curBid = null;
             var headerStatsInfo = {};
             var htSlotId = curReturnParcel.htSlot.getId();
+
             headerStatsInfo[htSlotId] = {};
             headerStatsInfo[htSlotId][curReturnParcel.requestId] = [curReturnParcel.xSlotName];
 
-            /* No response or no bids returned so it's a pass */
+            curBid = __getMatchingBid(curReturnParcel, adResponse);
 
-            if (!bids) {
+            if (!curBid) {
                 if (__profile.enabledAnalytics.requestTime) {
                     __baseClass._emitStatsEvent(sessionId, 'hs_slot_pass', headerStatsInfo);
                 }
                 curReturnParcel.pass = true;
-
-                return;
+                continue;
             }
 
-            var curBid;
+            /* ---------- Fill the bid variables with data from the bid response here. ------------ */
+            var bidPrice = curBid.price;
+            var bidSize = [Number(curBid.w), Number(curBid.h)];
+            var bidCreative = curBid.adm;
+            var bidDealId = curBid.dealid;
+            var pixelUrl = Browser.getProtocol() + '//biddr.brealtime.com/check.html?src=ix';
+            var bidIsPass = bidPrice <= 0;
+            /* --------------------------------------------------------------------------------------- */
 
-            for (var i = 0; i < bids.length; i++) {
-                /**
-                 * This section maps internal returnParcels and demand returned from the bid request.
-                 * In order to match them correctly, they must be matched via some criteria. This
-                 * is usually some sort of placements or inventory codes. Please replace the someCriteria
-                 * key to a key that represents the placement in the configuration and in the bid responses.
-                 */
-
-                /* ----------- Fill this out to find a matching bid for the current parcel ------------- */
-                if (curReturnParcel.requestId === bids[i].bid[0].id) {
-                    curBid = bids[i].bid[0];
-                    bids.splice(i, 1);
-
-                    break;
-                }
-            }
-
-            /* No matching bid found so its a pass */
-            if (!curBid) {
+            curBid = null;
+            if (bidIsPass) {
+                //? if (DEBUG) {
+                Scribe.info(__profile.partnerId + ' returned pass for { id: ' + adResponse.id + ' }.');
+                //? }
                 if (__profile.enabledAnalytics.requestTime) {
                     __baseClass._emitStatsEvent(sessionId, 'hs_slot_pass', headerStatsInfo);
                 }
@@ -320,32 +256,6 @@ function BRealTimeHtb(configs) {
                 continue;
             }
 
-            /* ---------- Fill the bid variables with data from the bid response here. ------------ */
-
-            /* Using the above variable, curBid, extract various information about the bid and assign it to
-             * these local variables */
-
-            /* The bid price for the given slot */
-            var bidPrice = curBid.price;
-
-            /* The size of the given slot */
-            var bidSize = [Number(curBid.w), Number(curBid.h)];
-
-            /* The creative/adm for the given slot that will be rendered if is the winner.
-                * Please make sure the URL is decoded and ready to be document.written.
-                */
-            var bidCreative = curBid.adm;
-
-            /* The dealId if applicable for this slot. */
-            var bidDealId = curBid.dealid;
-
-            /* OPTIONAL: tracking pixel url to be fired AFTER rendering a winning creative.
-                * If firing a tracking pixel is not required or the pixel url is part of the adm,
-                * leave empty;
-                */
-
-            /* --------------------------------------------------------------------------------------- */
-
             if (__profile.enabledAnalytics.requestTime) {
                 __baseClass._emitStatsEvent(sessionId, 'hs_slot_bid', headerStatsInfo);
             }
@@ -353,11 +263,12 @@ function BRealTimeHtb(configs) {
             curReturnParcel.size = bidSize;
             curReturnParcel.targetingType = 'slot';
             curReturnParcel.targeting = {};
+
             var targetingCpm = '';
 
             //? if (FEATURES.GPT_LINE_ITEMS) {
-            var sizeKey = Size.arrayToString(curReturnParcel.size);
             targetingCpm = __baseClass._bidTransformers.targeting.apply(bidPrice);
+            var sizeKey = Size.arrayToString(curReturnParcel.size);
 
             if (bidDealId) {
                 curReturnParcel.targeting[__baseClass._configs.targetingKeys.pmid] = [sizeKey + '_' + bidDealId];
@@ -370,6 +281,9 @@ function BRealTimeHtb(configs) {
 
             //? if (FEATURES.RETURN_CREATIVE) {
             curReturnParcel.adm = bidCreative;
+            if (pixelUrl) {
+                curReturnParcel.winNotice = __renderPixel.bind(null, pixelUrl);
+            }
             //? }
 
             //? if (FEATURES.RETURN_PRICE) {
@@ -382,10 +296,13 @@ function BRealTimeHtb(configs) {
                 adm: bidCreative,
                 requestId: curReturnParcel.requestId,
                 size: curReturnParcel.size,
-                price: targetingCpm ? targetingCpm : '',
-                dealId: bidDealId ? bidDealId : '',
-                timeOfExpiry: __profile.features.demandExpiry.enabled ? __profile.features.demandExpiry.value + System.now() : 0,  // eslint-disable-line
+                price: targetingCpm,
+                dealId: bidDealId || '',
+                timeOfExpiry: __profile.features.demandExpiry.enabled ? __profile.features.demandExpiry.value + System.now() : 0,
+                auxFn: __renderPixel,
+                auxArgs: [pixelUrl]
             });
+
 
             //? if (FEATURES.INTERNAL_RENDER) {
             curReturnParcel.targeting.pubKitAdId = pubKitAdId;
@@ -398,8 +315,8 @@ function BRealTimeHtb(configs) {
      * ---------------------------------- */
 
     (function __constructor() {
-        EventsService = SpaceCamp.services.EventsService;
         RenderService = SpaceCamp.services.RenderService;
+        EventsService = SpaceCamp.services.EventsService;
         ComplianceService = SpaceCamp.services.ComplianceService;
 
         /* =============================================================================
