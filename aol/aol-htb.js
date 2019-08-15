560b8c1e4591f9f9546925e5fb82ec4f2873d0be
slimkrazy <sam@slimkrazy.com>
2019-08-15 15:51:57 +0100

    Bid prices in dollars

--- aol/aol-htb.js
+++ aol/aol-htb.js
@@ -462,7 +462,7 @@ function AOLHtb(configs) {
             },
 
             /* The bid price unit (in cents) the endpoint returns, please refer to the readme for details */
-            bidUnitInCents: 1,
+            bidUnitInCents: 100,
             lineItemType: Constants.LineItemTypes.ID_AND_SIZE,
             callbackType: Partner.CallbackTypes.NONE,
             architecture: Partner.Architectures.MRA,
