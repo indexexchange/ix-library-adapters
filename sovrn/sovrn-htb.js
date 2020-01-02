0f04cadae6b0e851bd01f67131f2da243803b22d
Ankit Prakash <aprakash@sovrn.com>
2020-01-02 10:26:10 -0700

    lint fix

--- sovrn/sovrn-htb.js
+++ sovrn/sovrn-htb.js
@@ -151,8 +151,8 @@ function SovrnHtb(configs) {
 
             var uspStatus = ComplianceService.usp.getConsent();
             if (uspStatus) {
-                br.regs = br.regs || {}
-                br.regs.ext = br.regs.ext || {}
+                br.regs = br.regs || {};
+                br.regs.ext = br.regs.ext || {};
                 // eslint-disable-next-line camelcase
                 br.regs.ext.us_privacy = uspStatus.uspString;
             }
