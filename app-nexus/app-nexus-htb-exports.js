6d5f0c653b2bc2f6fd5c2bb05dc43305803d7d1e
Jason Snellbaker <jsnellbaker@appnexus.com>
2020-01-15 14:19:22 -0500

    update appnexus bidder

--- app-nexus/app-nexus-htb-exports.js
+++ app-nexus/app-nexus-htb-exports.js
@@ -7,8 +7,3 @@ shellInterface.AppNexusHtb = {
 window.pbjs = window.pbjs || {};
 window.pbjs.renderApnxAd = SpaceCamp.services.RenderService.renderDfpAd.bind(null, 'AppNexusHtb');
 //? }
-
-if (__directInterface.Layers.PartnersLayer.Partners.AppNexusHtb) {
-    shellInterface.AppNexusHtb = shellInterface.AppNexusHtb || {};
-    shellInterface.AppNexusHtb.adResponseCallback = __directInterface.Layers.PartnersLayer.Partners.AppNexusHtb.adResponseCallback;
-}
