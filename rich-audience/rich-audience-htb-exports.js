d136b7b1ed101ecfc0cabf7b6d5c3ff3df5a1002
Sergi Navarro <cert@richaudience.com>
2020-04-16 14:01:30 +0200

    Inital commit

---
+++ rich-audience/rich-audience-htb-exports.js
@@ -0,0 +1,5 @@
+//? if (FEATURES.GPT_LINE_ITEMS) {
+shellInterface.RichAudienceHtb = {
+    render: SpaceCamp.services.RenderService.renderDfpAd.bind(null, 'RichAudienceHtb')
+};
+//? }
---
+++ rich-audience/rich-audience-htb.js
@@ -0,0 +1,410 @@
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// Dependencies ////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+var Browser = require('browser.js');
+var Classify = require('classify.js');
+var Constants = require('constants.js');
+var Partner = require('partner.js');
+var Size = require('size.js');
+var SpaceCamp = require('space-camp.js');
+var System = require('system.js');
+var Network = require('network.js');
+var Utilities = require('utilities.js');
+
+var ComplianceService;
+var RenderService;
+
+//? if (DEBUG) {
+var ConfigValidators = require('config-validators.js');
+var PartnerSpecificValidator = require('rich-audience-htb-validator.js');
+var Scribe = require('scribe.js');
+var Whoopsie = require('whoopsie.js');
+//? }
+
+////////////////////////////////////////////////////////////////////////////////
+// Main ////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * Partner module template
+ *
+ * @class
+ */
+function RichAudienceHtb(configs) {
+    /* =====================================
+     * Data
+     * ---------------------------------- */
+
+    /* Private
+     * ---------------------------------- */
+
+    /**
+     * Reference to the partner base class.
+     *
+     * @private {object}
+     */
+    var __baseClass;
+
+    /**
+     * Profile for this partner.
+     *
+     * @private {object}
+     */
+    var __profile;
+
+    /* =====================================
+     * Functions
+     * ---------------------------------- */
+
+    function raiGetParcels(returnParcels, queryObj) {
+        if (!Utilities.isEmpty(returnParcels)) {
+            returnParcels.forEach(function (parcel) {
+                queryObj.pid = parcel.xSlotRef.placementId;
+                queryObj.sizes = parcel.xSlotRef.sizes.map(function (size) {
+                    return {
+                        w: size[0],
+                        h: size[1]
+                    };
+                });
+                queryObj.supplyType = parcel.xSlotRef.supplyType;
+                queryObj.tagId = parcel.xSlotRef.adUnitName;
+                queryObj.bidder = parcel.partnerId;
+                queryObj.bidId = parcel.partnerStatsId;
+                queryObj.bidderRequestId = parcel.requestId;
+                queryObj.xSlotName = parcel.xSlotName;
+            });
+        }
+    }
+
+    function raiGetGDPR(queryObj) {
+        var privacyEnabled = ComplianceService.isPrivacyEnabled();
+
+        if (privacyEnabled) {
+            var gdprStatus = ComplianceService.gdpr.getConsent();
+            if (Utilities.isEmpty(gdprStatus.consentString) !== 'undefined') {
+                queryObj.gdprConsent = gdprStatus.consentString;
+                queryObj.gdpr = gdprStatus.applies;
+            } else {
+                queryObj.gdprConsent = 'undefined';
+                queryObj.gdpr = false;
+            }
+        }
+    }
+
+    /* Utilities
+     * ---------------------------------- */
+
+    /**
+     * Generates the request URL and query data to the endpoint for the xSlots
+     * in the given returnParcels.
+     *
+     * @param  {object[]} returnParcels
+     *
+     * @return {object}
+     */
+    function __generateRequestObj(returnParcels) {
+        var queryObj = {};
+        var callbackId = System.generateUniqueId();
+        var baseUrl = 'https://shb.richaudience.com/hb/';
+
+        queryObj = {
+            currencyCode: configs.currencyCode,
+            referer: encodeURIComponent(Browser.getPageUrl()),
+            adapter: 'IX'
+        };
+
+        queryObj.raiParcels = raiGetParcels(returnParcels, queryObj);
+
+        raiGetGDPR(queryObj);
+
+        return {
+            url: baseUrl,
+            data: queryObj,
+            callbackId: callbackId,
+            networkParamOverrides: {
+                method: 'POST',
+                contentType: 'text/plain'
+            }
+        };
+    }
+
+    /* =============================================================================
+     * STEP 3  | Response callback
+     * -----------------------------------------------------------------------------
+     */
+    function adResponseCallback(adResponse) {
+        /* Get callbackId from adResponse here */
+        var callbackId = 0;
+        __baseClass._adResponseStore[callbackId] = adResponse;
+    }
+
+    /* -------------------------------------------------------------------------- */
+
+    /* Helpers
+     * ---------------------------------- */
+
+    /* =============================================================================
+     * STEP 5  | Rendering Pixel
+     * -----------------------------------------------------------------------------
+     *
+    */
+
+    /**
+     * This function will render the pixel given.
+     * @param  {string} pixelUrl Tracking pixel img url.
+     */
+    function __renderPixel(pixelUrl) {
+        if (pixelUrl) {
+            Network.img({
+                url: decodeURIComponent(pixelUrl),
+                method: 'GET'
+            });
+        }
+    }
+
+    /**
+     * Parses and extracts demand from adResponse according to the adapter and then attaches it
+     * to the corresponding bid's returnParcel in the correct format using ºg keys.
+     *
+     * @param {string} sessionId The sessionId, used for stats and other events.
+     *
+     * @param {any} adResponse This is the bid response as returned from the bid request, that was either
+     * passed to a JSONP callback or simply sent back via AJAX.
+     *
+     * @param {object[]} returnParcels The array of original parcels, SAME array that was passed to
+     * generateRequestObj to signal which slots need demand. In this funciton, the demand needs to be
+     * attached to each one of the objects for which the demand was originally requested for.
+     */
+    function __parseResponse(sessionId, adResponse, returnParcels) {
+        /* =============================================================================
+         * STEP 4  | Parse & store demand response
+         * -----------------------------------------------------------------------------
+         */
+
+        /*
+        *
+        *
+
+         ---------- Process adResponse and extract the bids into the bids array ------------ */
+        var bids = adResponse;
+
+        for (var j = 0; j < returnParcels.length; j++) {
+            var curReturnParcel = returnParcels[j];
+
+            var headerStatsInfo = {};
+            var htSlotId = curReturnParcel.htSlot.getId();
+            headerStatsInfo[htSlotId] = {};
+            headerStatsInfo[htSlotId][curReturnParcel.requestId] = [curReturnParcel.xSlotName];
+
+            var bidPrice = bids.cpm;
+
+            /* The size of the given slot */
+            var bidSize = [Number(bids.width), Number(bids.height)];
+
+            /* The creative/adm for the given slot that will be rendered if is the winner.
+             * Please make sure the URL is decoded and ready to be document.written.
+             */
+            var bidCreative = bids.adm;
+
+            /* The dealId if applicable for this slot. */
+            var bidDealId = bids.dealId;
+
+            /* Explicitly pass */
+            var bidIsPass = bidPrice <= 0;
+
+            /* OPTIONAL: tracking pixel url to be fired AFTER rendering a winning creative.
+            * If firing a tracking pixel is not required or the pixel url is part of the adm,
+            * leave empty;
+            */
+
+            var pixelUrl = 'https://sync.richaudience.com/bf7c142f4339da0278e83698a02b0854/';
+            pixelUrl += '?referrer=' + encodeURIComponent(Browser.getPageUrl());
+
+            var gdprStatus = ComplianceService.gdpr.getConsent();
+            if (Utilities.isEmpty(gdprStatus.consentString) !== 'undefined') {
+                pixelUrl += '&euconsent=' + gdprStatus.consentString;
+            }
+
+            /* --------------------------------------------------------------------------------------- */
+
+            if (bidIsPass) {
+                //? if (DEBUG) {
+                Scribe.info(__profile.partnerId + ' returned pass for { id: ' + adResponse.id + ' }.');
+                //? }
+                if (__profile.enabledAnalytics.requestTime) {
+                    __baseClass._emitStatsEvent(sessionId, 'hs_slot_pass', headerStatsInfo);
+                }
+                curReturnParcel.pass = true;
+
+                continue;
+            }
+
+            if (__profile.enabledAnalytics.requestTime) {
+                __baseClass._emitStatsEvent(sessionId, 'hs_slot_bid', headerStatsInfo);
+            }
+
+            curReturnParcel.size = bidSize;
+            curReturnParcel.targetingType = 'slot';
+            curReturnParcel.targeting = {};
+
+            var targetingCpm = '';
+
+            //? if (FEATURES.GPT_LINE_ITEMS) {
+            targetingCpm = __baseClass._bidTransformers.targeting.apply(bidPrice);
+            var sizeKey = Size.arrayToString(curReturnParcel.size);
+
+            if (bidDealId) {
+                curReturnParcel.targeting[__baseClass._configs.targetingKeys.pmid] = [sizeKey + '_' + bidDealId];
+                curReturnParcel.targeting[__baseClass._configs.targetingKeys.pm] = [sizeKey + '_' + targetingCpm];
+            } else {
+                curReturnParcel.targeting[__baseClass._configs.targetingKeys.om] = [sizeKey + '_' + targetingCpm];
+            }
+            curReturnParcel.targeting[__baseClass._configs.targetingKeys.id] = [curReturnParcel.requestId];
+
+            //? }
+
+            //? if (FEATURES.RETURN_CREATIVE) {
+            curReturnParcel.adm = bidCreative;
+            if (pixelUrl) {
+                curReturnParcel.winNotice = __renderPixel.bind(null, pixelUrl);
+            }
+            //? }
+
+            //? if (FEATURES.RETURN_PRICE) {
+            curReturnParcel.price = Number(__baseClass._bidTransformers.price.apply(bidPrice));
+            //? }
+
+            var expiry = 0;
+            if (__profile.features.demandExpiry.enabled) {
+                expiry = __profile.features.demandExpiry.value + System.now();
+            }
+
+            var pubKitAdId = RenderService.registerAd({
+                sessionId: sessionId,
+                partnerId: __profile.partnerId,
+                adm: bidCreative,
+                requestId: curReturnParcel.requestId,
+                size: curReturnParcel.size,
+                price: targetingCpm,
+                dealId: bidDealId || null,
+                timeOfExpiry: expiry,
+                auxFn: __renderPixel,
+                auxArgs: [pixelUrl]
+            });
+
+            //? if (FEATURES.INTERNAL_RENDER) {
+            curReturnParcel.targeting.pubKitAdId = pubKitAdId;
+            //? }
+        }
+    }
+
+    /* =====================================
+     * Constructors
+     * ---------------------------------- */
+
+    (function __constructor() {
+        ComplianceService = SpaceCamp.services.ComplianceService;
+        RenderService = SpaceCamp.services.RenderService;
+
+        /* =============================================================================
+         * STEP 1  | Partner Configuration
+         * -----------------------------------------------------------------------------
+         *
+         * Please fill out the below partner profile according to the steps in the README doc.
+         */
+
+        /* ---------- Please fill out this partner profile according to your module ------------ */
+        __profile = {
+            partnerId: 'RichAudienceHtb',
+            namespace: 'RichAudienceHtb',
+            statsId: 'RIC',
+            version: '2.1.0',
+            targetingType: 'slot',
+            enabledAnalytics: {
+                requestTime: false
+            },
+            features: {
+                demandExpiry: {
+                    enabled: false,
+                    value: 0
+                },
+                rateLimiting: {
+                    enabled: false,
+                    value: 0
+                }
+            },
+
+            /* Targeting keys for demand, should follow format ix_{statsId}_id */
+            targetingKeys: {
+                id: 'ix_ric_id',
+                om: 'ix_ric_om',
+                pm: 'ix_ric_om',
+                pmid: 'ix_ric_dealid'
+            },
+
+            /* The bid price unit (in cents) the endpoint returns, please refer to the readme for details */
+            bidUnitInCents: 100,
+            lineItemType: Constants.LineItemTypes.ID_AND_SIZE,
+            callbackType: Partner.CallbackTypes.NONE,
+            architecture: Partner.Architectures.MRA,
+            requestType: Partner.RequestTypes.ANY
+        };
+
+        /* --------------------------------------------------------------------------------------- */
+        //? if (DEBUG) {
+        var results = ConfigValidators.partnerBaseConfig(configs) || PartnerSpecificValidator(configs);
+
+        if (results) {
+            throw Whoopsie('INVALID_CONFIG', results);
+        }
+        //? }
+        __baseClass = Partner(__profile, configs, null, {
+            parseResponse: __parseResponse,
+            generateRequestObj: __generateRequestObj,
+            adResponseCallback: adResponseCallback
+        });
+    })();
+
+    /* =====================================
+     * Public Interface
+     * ---------------------------------- */
+    var derivedClass = {
+
+        /* Class Information
+         * ---------------------------------- */
+
+        //? if (DEBUG) {
+        __type__: 'RichAudienceHtb',
+        //? }
+
+        //? if (TEST) {
+        __baseClass: __baseClass,
+        //? }
+
+        /* Data
+         * ---------------------------------- */
+
+        //? if (TEST) {
+        profile: __profile,
+        //? }
+
+        /* Functions
+         * ---------------------------------- */
+
+        //? if (TEST) {
+        parseResponse: __parseResponse,
+        generateRequestObj: __generateRequestObj,
+        adResponseCallback: adResponseCallback
+        //? }
+    };
+
+    return Classify.derive(__baseClass, derivedClass);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Exports /////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports = RichAudienceHtb;
