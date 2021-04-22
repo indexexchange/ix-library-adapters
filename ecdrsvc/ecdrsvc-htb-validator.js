06103e03b08fc8a283e16e72ab7f120c39211f42
john.buco <john.buco@loblaw.ca>
2021-04-22 11:32:37 -0400

    implement validator

--- ecdrsvc/ecdrsvc-htb-validator.js
+++ ecdrsvc/ecdrsvc-htb-validator.js
@@ -1,24 +1,7 @@
 'use strict';
 
-////////////////////////////////////////////////////////////////////////////////
-// Dependencies ////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
 var Inspector = require('../../../libs/external/schema-inspector.js');
 
-////////////////////////////////////////////////////////////////////////////////
-// Main ////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-/* =============================================================================
- * STEP 0 | Config Validation
- * -----------------------------------------------------------------------------
- * This file contains the necessary validation for the partner configuration.
- * This validation will be performed on the partner specific configuration object
- * that is passed into the wrapper. The wrapper uses an outside library called
- * schema-insepctor to perform the validation. Information about it can be found here:
- * https://atinux.fr/schema-inspector/.
- */
 function partnerValidator(configs) {
     var result = Inspector.validate({
         type: 'object',
@@ -29,9 +12,19 @@ function partnerValidator(configs) {
                     '*': {
                         type: 'object',
                         properties: {
-                            placementId: {
-                                type: 'string',
-                                minLength: 1
+                            ppid: {
+                                type: 'integer'
+                            },
+                            sizes: {
+                                type: 'array',
+                                minLength: 1,
+                                items: {
+                                    type: 'array',
+                                    exactLength: 2,
+                                    items: {
+                                        type: 'integer'
+                                    }
+                                }
                             }
                         }
                     }
